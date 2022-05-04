83a29b03743d7fd35430f38c488ba5f5bdb8afaf
pabtejada <u201523331@upc.edu.pe>
2022-05-04 17:30:51 -0500

    login activity

--- app/src/main/java/com/example/asimov/MainActivity.java
+++ app/src/main/java/com/example/asimov/MainActivity.java
@@ -1,14 +1,46 @@
 package com.example.asimov;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.example.asimov.databinding.ActivityMainBinding;
+import com.example.asimov.R;
+
+public class MainActivity extends Fragment {
+    private ActivityMainBinding binding;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+
+        binding = ActivityMainBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.buttonSecond.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(MainActivity.this)
+                        .navigate(R.id.action_MainActivity_to_LoginFragment);
+            }
+        });
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
     }
 }
