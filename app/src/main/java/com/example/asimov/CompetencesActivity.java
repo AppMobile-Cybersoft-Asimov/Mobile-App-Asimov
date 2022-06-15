38166b0a33faf2c7475709ea3af86bcb82982f76
DiegoJohnsonL <diegojohnson46@gmail.com>
2022-06-15 11:13:43 -0500

    token

--- app/src/main/java/com/example/asimov/CompetencesActivity.java
+++ app/src/main/java/com/example/asimov/CompetencesActivity.java
@@ -13,6 +13,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
+import com.example.asimov.data.RetrofitClient;
 import com.example.asimov.databinding.ActivityAnnouncementsBinding;
 import com.example.asimov.databinding.ActivityCompetencesBinding;
 import com.example.asimov.databinding.ActivityDashboardDirectorBinding;
@@ -34,7 +35,7 @@ public class CompetencesActivity extends Fragment {
     private ActivityCompetencesBinding binding;
 
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityCompetencesBinding.inflate(getLayoutInflater());
         binding.rvCompetences.setLayoutManager(new LinearLayoutManager(
@@ -51,39 +52,34 @@ public class CompetencesActivity extends Fragment {
         super.onCreate(savedInstanceState);
     }
 
-    private void retrieveCompetences(){
+    private void retrieveCompetences() {
 
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("https://asimov.azurewebsites.net/")
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
-
-        AsimovApi asimovApi = retrofit.create(AsimovApi.class);
+        AsimovApi asimovApi = RetrofitClient.createInstance().create(AsimovApi.class);
         Call<List<Competence>> inter = asimovApi.getCompetences();
 
         inter.enqueue(new Callback<List<Competence>>() {
             @Override
             public void onResponse(Call<List<Competence>> call, Response<List<Competence>> response) {
-                if(!response.isSuccessful()){
-                    Toast.makeText(getContext(), "Codigo de error: "+ response.code(), Toast.LENGTH_SHORT).show();
-                    generateCompetences();
+                if (!response.isSuccessful()) {
+                    Toast.makeText(getContext(), "Codigo de error: " + response.code(), Toast.LENGTH_SHORT).show();
+                    return;
                 }
-                else {
-                    List<Competence> listCompetences = response.body();
-                    ListCompetencesAdapter listCompetencesAdapter = new ListCompetencesAdapter(listCompetences);
-                    binding.rvCompetences.setAdapter(listCompetencesAdapter);
-                }
+                List<Competence> listCompetences = response.body();
+                ListCompetencesAdapter listCompetencesAdapter = new ListCompetencesAdapter(listCompetences);
+                binding.rvCompetences.setAdapter(listCompetencesAdapter);
             }
+
             @Override
             public void onFailure(Call<List<Competence>> call, Throwable t) {
 
             }
         });
     }
-    private void generateCompetences(){
+
+    private void generateCompetences() {
         List<Competence> listCompetences = new ArrayList<>();
-        for(int i = 0; i <= 5; i++){
-            Competence competence = new Competence(i,"Competencia " + Integer.toString(i+1), "descripcion");
+        for (int i = 0; i <= 5; i++) {
+            Competence competence = new Competence(i, "Competencia " + Integer.toString(i + 1), "descripcion");
             listCompetences.add(competence);
         }
         ListCompetencesAdapter listCompetencesAdapter = new ListCompetencesAdapter(listCompetences);
