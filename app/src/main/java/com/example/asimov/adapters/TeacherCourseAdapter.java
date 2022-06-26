482f81fedbd44e08bc0a5811ab9d9e1a71fb8336
S-011 <u201811286@upc.edu.pe>
2022-06-25 21:54:20 -0500

    fixed and added new methods
    renamed id's, fixed errors and added new methods

--- app/src/main/java/com/example/asimov/adapters/TeacherCourseAdapter.java
+++ app/src/main/java/com/example/asimov/adapters/TeacherCourseAdapter.java
@@ -4,25 +4,22 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
-
 import com.example.asimov.R;
-
 import java.util.List;
 import com.example.asimov.data.model.Courses;
 
-public class CourseAdapter extends RecyclerView.Adapter<CourseAdapter.viewHolder> {
+public class TeacherCourseAdapter extends RecyclerView.Adapter<TeacherCourseAdapter.viewHolder> {
 
     private List<Courses> data;
-    public CourseAdapter(List<Courses> data) {this.data = data;}
+    public TeacherCourseAdapter(List<Courses> data) {this.data = data;}
 
     @NonNull
     @Override
     public viewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.frag_course,null, false);
+                .inflate(R.layout.card_teacher_course,null, false);
 
         view.setLayoutParams(new RecyclerView.LayoutParams(RecyclerView.LayoutParams.MATCH_PARENT, RecyclerView.LayoutParams.WRAP_CONTENT));
         return new viewHolder(view);
@@ -39,21 +36,17 @@ public class CourseAdapter extends RecyclerView.Adapter<CourseAdapter.viewHolder
     }
 
     public class viewHolder extends RecyclerView.ViewHolder {
-        TextView id, name, description, state;
+        TextView name, state;
 
         public viewHolder(@NonNull View itemView) {
             super(itemView);
-            //id = itemView.findViewById(R.id.txtCId);
-            name = itemView.findViewById(R.id.txtCName);
-            //description = itemView.findViewById(R.id.txtCDescription);
-            state = itemView.findViewById(R.id.txtCState);
+            name = itemView.findViewById(R.id.txtTCName);
+            state = itemView.findViewById(R.id.txtTCState);
         }
 
 
         public void asignData(Courses courses) {
-            //id.setText(courses.getId()+"");
             name.setText("Course: " + courses.getName());
-            //description.setText(courses.getDescription()+"");
             if (courses.getState()) {
                 state.setText("Completed");
             }
