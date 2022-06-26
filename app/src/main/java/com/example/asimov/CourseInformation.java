0540f8531fc628987e77129842a42f014432c173
AldairCB <aldaircuarez98@gmail.com>
2022-06-26 14:51:50 -0500

    course info update

--- app/src/main/java/com/example/asimov/CourseInformation.java
+++ app/src/main/java/com/example/asimov/CourseInformation.java
@@ -5,20 +5,27 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.asimov.data.RetrofitClient;
+import com.example.asimov.data.model.Announcement;
 import com.example.asimov.data.model.Competence;
+import com.example.asimov.data.model.CourseItem;
 import com.example.asimov.data.model.Courses;
+import com.example.asimov.data.service.AnnouncementsService;
 import com.example.asimov.data.service.AsimovApi;
+import com.example.asimov.data.service.CourseService;
 import com.example.asimov.databinding.ActivityCourseInformationBinding;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import Adapters.CourseCompetencesAdapter;
+import Adapters.CourseItemsAdapter;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -29,6 +36,7 @@ public class CourseInformation extends Fragment {
 
     //private Courses course = new Courses();
     private List<Competence> competencesList;
+    private List<CourseItem> courseItemsList;
     private ActivityCourseInformationBinding binding;
 
     @Override
@@ -36,56 +44,74 @@ public class CourseInformation extends Fragment {
         super.onCreate(savedInstanceState);
         binding = ActivityCourseInformationBinding.inflate(getLayoutInflater());
 
-        getCourseById(0);
+        getCourseById(1);
+
         getCompetences();
+        getCourseItems(1);
 
         return binding.getRoot();
     }
     private void getCourseById(int courseId){
-        AsimovApi asimovApi = RetrofitClient.createInstance().create(AsimovApi.class);
-        Call<Courses> inter = asimovApi.getCourseById(courseId);
+        CourseService courseService = RetrofitClient.createInstanceWithoutToken().create(CourseService.class);
+        Call<Courses> inter = courseService.getCourseById(courseId);
 
         inter.enqueue(new Callback<Courses>() {
             @Override
             public void onResponse(Call<Courses> call, Response<Courses> response) {
-                if(!response.isSuccessful()) {
-                    binding.lblCourseName.setText("Codigo de error: "+response.code());
-                    return;
-                }
+
+                binding.lblCourseName.setText("Codigo de error: ");
+                Log.d("", response.body()+"");
                 Courses course = response.body();
-                System.out.print(course.getName());
                 binding.lblCourseName.setText(course.getName());
                 binding.txtCourseDescription.setText(course.getDescription());
-
             }
-
             @Override
             public void onFailure(Call<Courses> call, Throwable t) { }
         });
     }
     private void getCompetences(){
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("https://asimov.azurewebsites.net/api/v1/")
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
-        AsimovApi asimovApi = retrofit.create(AsimovApi.class);
-        Call<List<Competence>> inter = asimovApi.getCompetences();
-
-        inter.enqueue(new Callback<List<Competence>>() {
+        AsimovApi asimovApi = RetrofitClient.createInstanceWithoutToken().create(AsimovApi.class);
+        asimovApi.getCompetences().enqueue(new Callback<List<Competence>>() {
             @Override
             public void onResponse(Call<List<Competence>> call, Response<List<Competence>> response) {
-                List<Competence> list = response.body();
-                competencesList = new ArrayList<Competence>();
-                //for(Competence co:list){
-                //    competencesList.add(new Competence(co.getId(), co.getTitle(),
-                //            co.getDescription()));
-                //}
-                //CourseCompetencesAdapter adapter = new CourseCompetencesAdapter(competencesList);
-                //binding.rvCourseCompetences.setAdapter(adapter);
+                if (!response.isSuccessful()) {
+                    Toast.makeText(getContext(), "Error loading Competences" + response.errorBody(), Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                List<Competence> competences = response.body();
+                System.out.println(competences);
+
+                CourseCompetencesAdapter adapter = new CourseCompetencesAdapter(competences);
+                binding.rvCourseCompetences.setAdapter(adapter);
             }
 
             @Override
-            public void onFailure(Call<List<Competence>> call, Throwable t) { }
+            public void onFailure(Call<List<Competence>> call, Throwable t) {
+                Toast.makeText(getContext(), "Error connecting to the server", Toast.LENGTH_SHORT).show();
+            }
         });
     }
+
+    private void getCourseItems(int courseId){
+        CourseService courseService = RetrofitClient.createInstance().create(CourseService.class);
+        courseService.getCourseItems(courseId).enqueue(new Callback<List<CourseItem>>() {
+            @Override
+            public void onResponse(Call<List<CourseItem>> call, Response<List<CourseItem>> response) {
+                if (!response.isSuccessful()) {
+                    Toast.makeText(getContext(), "Error loading Competences" + response.errorBody(), Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                List<CourseItem> courseItemsList = response.body();
+
+                CourseItemsAdapter adapter = new CourseItemsAdapter(courseItemsList);
+                binding.rvCourseCompetences.setAdapter(adapter);
+            }
+
+            @Override
+            public void onFailure(Call<List<CourseItem>> call, Throwable t) {
+                Toast.makeText(getContext(), "Error connecting to the server", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
 }
---
+++ app/src/main/java/com/example/asimov/data/service/CourseService.java
@@ -0,0 +1,19 @@
+package com.example.asimov.data.service;
+
+import com.example.asimov.data.model.Competence;
+import com.example.asimov.data.model.CourseItem;
+import com.example.asimov.data.model.Courses;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+public interface CourseService {
+    @GET("/api/v1/courses/{courseId}/items")
+    Call<List<CourseItem>> getCourseItems(@Path("id") int id);
+
+    @GET("/api/v1/courses/{id}")
+    Call<Courses> getCourseById(@Path("id") int id);
+}
