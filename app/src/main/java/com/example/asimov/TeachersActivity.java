0384390438919ee807717ad3f2f240205509f0ba
Nicolas Hernandez <u201011264@upc.edu.pe>
2022-06-27 22:22:26 +0200

    chore: Teacher list call the API

--- app/src/main/java/com/example/asimov/TeachersActivity.java
+++ app/src/main/java/com/example/asimov/TeachersActivity.java
@@ -5,16 +5,23 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
+import com.example.asimov.data.RetrofitClient;
 import com.example.asimov.data.model.Teacher;
+import com.example.asimov.data.service.TeacherService;
+import com.example.asimov.manager.SessionManager;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class TeachersActivity extends AppCompatActivity {
 
     RecyclerView recycler;
-    List<Teacher> lista;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,11 +35,26 @@ public class TeachersActivity extends AppCompatActivity {
     }
 
     private void getTeachers() {
-        List<Teacher> teacher = new ArrayList<>();
-        for (int i = 1; i < 50; i++) {
-            teacher.add(new Teacher((long) i, "Email " + i, "Name " + i, "Phone " + i));
-        }
-        TeachersAdapter adapter = new TeachersAdapter(teacher);
-        recycler.setAdapter(adapter);
+        TeacherService teacherService = RetrofitClient.createInstance().create(TeacherService.class);
+        teacherService.getTeachersByDirector(SessionManager.getInstance().getUserId()).enqueue(new Callback<List<Teacher>>() {
+            @Override
+            public void onResponse(Call<List<Teacher>> call, Response<List<Teacher>> response) {
+                if (!response.isSuccessful()) {
+                    Toast.makeText(getApplicationContext(), "Error loading Announcement" + response.errorBody(), Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                List<Teacher> lista = response.body();
+                System.out.println(lista);
+                TeachersAdapter adapter = new TeachersAdapter(lista);
+                recycler.setAdapter(adapter);
+            }
+
+            @Override
+            public void onFailure(Call<List<Teacher>> call, Throwable t) {
+                Toast.makeText(getApplicationContext(), "Error connecting to the server", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
     }
 }
--- app/src/main/java/com/example/asimov/data/service/TeacherService.java
+++ app/src/main/java/com/example/asimov/data/service/TeacherService.java
@@ -4,10 +4,15 @@ import com.example.asimov.data.model.LoginRequest;
 import com.example.asimov.data.model.LoginResponse;
 import com.example.asimov.data.model.RegisterRequest;
 import com.example.asimov.data.model.RegisterResponse;
+import com.example.asimov.data.model.Teacher;
+
+import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.Path;
 
 public interface TeacherService {
 
@@ -15,5 +20,6 @@ public interface TeacherService {
     Call<LoginResponse> login(@Body LoginRequest login);
     @POST("auth/sign-up/teacher")
     Call<RegisterResponse> register(@Body RegisterRequest login);
-
+    @GET("/api/v1/directors/{directorId}/teachers")
+    Call<List<Teacher>> getTeachersByDirector(@Path("directorId") Integer id);
 }
